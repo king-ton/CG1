ade226b00f5b2cea1181fe3e7bc7158369b3dbeb
king-ton <tfreitag93@gmail.com>
2014-01-27 21:01:15 +0100

    Übung5 - Aufgabe2b - Rückseiten von Primitiven werden nicht gezeichnet

--- CGContext_PrimitiveOperations.cpp
+++ CGContext_PrimitiveOperations.cpp
@@ -39,13 +39,25 @@ int CGContext::m_cgClipPrimitive()
 	// Returns the new primitive count
 	return newPrimCount;
 }
+
+//---------------------------------------------------------------------------
+// Übung 5 - Aufgabe 2b  |  Rückseiten von Primitiven werden nicht gezeichnet
 //---------------------------------------------------------------------------
 bool CGContext::m_cgBFCullTriangle(CGVec4 &vertex0Position,
 	CGVec4 &vertex1Position,
 	CGVec4 &vertex2Position)
 {
-	// ...
-	return false; // Not culled.
+	float ax = vertex0Position.elements[0];
+	float ay = vertex0Position.elements[1];
+	float bx = vertex1Position.elements[0];
+	float by = vertex1Position.elements[1];
+	float cx = vertex2Position.elements[0];
+	float cy = vertex2Position.elements[1];
+
+	if (((bx - ax) * (cy - ay) - (by - ay) * (cx - ax)) >= 0)
+		return false;
+	else
+		return true;
 }
 //---------------------------------------------------------------------------
 // CGCONTEXT RASTERIZERS
@@ -120,11 +132,20 @@ void CGContext::m_cgRasterizeLineBresenham(int pipelineVertexOffset)
 
 //---------------------------------------------------------------------------
 // Übung 03 - Aufgabe 2a  |  Funktion implementiert
+// Übung 05 - Aufgabe 2b  |  Rückseiten von Primitiven werden nicht gezeichnet
 //---------------------------------------------------------------------------
 void CGContext::m_cgRasterizeWireTriangle(int pipelineVertexOffset)
 {
-	for (int i = 0; i < 3; i++) {
-		m_cgRasterizeLine(pipelineVertexOffset + i, pipelineVertexOffset + (i + 1) % 3);
+	CGVertexVaryings *vertex = m_pipelineVertexVaryings + pipelineVertexOffset;
+
+	CGVec4 &vertex0 = vertex[0].varyings[CG_POSITION_VARYING];
+	CGVec4 &vertex1 = vertex[1].varyings[CG_POSITION_VARYING];
+	CGVec4 &vertex2 = vertex[2].varyings[CG_POSITION_VARYING];
+
+	if (m_cgBFCullTriangle(vertex0, vertex1, vertex2) == false) {
+		for (int i = 0; i < 3; i++) {
+			m_cgRasterizeLine(pipelineVertexOffset + i, pipelineVertexOffset + (i + 1) % 3);
+		}
 	}
 }
 
@@ -132,6 +153,7 @@ void CGContext::m_cgRasterizeWireTriangle(int pipelineVertexOffset)
 // Übung 04 - Aufgabe 1a  |  Funktion implementiert
 // Übung 04 - Aufgabe 2b  |  Interpolation hinzugefügt
 // Übung 04 - Aufgabe 3   |  Optimierung
+// Übung 05 - Aufgabe 2b  |  Rückseiten von Primitiven werden nicht gezeichnet
 //---------------------------------------------------------------------------
 void CGContext::m_cgRasterizeTriangle(int pipelineVertexOffset)
 {
@@ -142,46 +164,48 @@ void CGContext::m_cgRasterizeTriangle(int pipelineVertexOffset)
 	CGVec4 &vertex1 = B.varyings[CG_POSITION_VARYING];
 	CGVec4 &vertex2 = C.varyings[CG_POSITION_VARYING];
 
-	CGFragmentData fragment;
+	if (m_cgBFCullTriangle(vertex0, vertex1, vertex2) == false) {
+		CGFragmentData fragment;
 
-	float det = (vertex1[X] - vertex0[X]) * (vertex2[Y] - vertex0[Y]) -
-				(vertex1[Y] - vertex0[Y]) * (vertex2[X] - vertex0[X]);
+		float det = (vertex1[X] - vertex0[X]) * (vertex2[Y] - vertex0[Y]) -
+			(vertex1[Y] - vertex0[Y]) * (vertex2[X] - vertex0[X]);
 
-	int minX = m_frameBuffer.getWidth(),
-		minY = m_frameBuffer.getHeight(),
-		maxX = -1,
-		maxY = -1;
+		int minX = m_frameBuffer.getWidth(),
+			minY = m_frameBuffer.getHeight(),
+			maxX = -1,
+			maxY = -1;
 
-	for (int i = 0; i < 3; ++i) {
-		int x = vertex[i].varyings[CG_POSITION_VARYING][X],
-			y = vertex[i].varyings[CG_POSITION_VARYING][Y];
+		for (int i = 0; i < 3; ++i) {
+			int x = vertex[i].varyings[CG_POSITION_VARYING][X],
+				y = vertex[i].varyings[CG_POSITION_VARYING][Y];
 
-		if (x < minX) minX = x;
-		if (x > maxX) maxX = x;
-		if (y < minY) minY = y;
-		if (y > maxY) maxY = y;
-	}
+			if (x < minX) minX = x;
+			if (x > maxX) maxX = x;
+			if (y < minY) minY = y;
+			if (y > maxY) maxY = y;
+		}
 
 
-	for (int iY = minY; iY <= maxY; iY++)
-	for (int iX = minX; iX <= maxX; iX++) {
-		float x = iX + 0.5F, y = iY + 0.5F;
+		for (int iY = minY; iY <= maxY; iY++)
+		for (int iX = minX; iX <= maxX; iX++) {
+			float x = iX + 0.5F, y = iY + 0.5F;
 
-		float x0 = vertex0[X] - x;
-		float y0 = vertex0[Y] - y;
-		float x1 = vertex1[X] - x;
-		float y1 = vertex1[Y] - y;
-		float x2 = vertex2[X] - x;
-		float y2 = vertex2[Y] - y;
+			float x0 = vertex0[X] - x;
+			float y0 = vertex0[Y] - y;
+			float x1 = vertex1[X] - x;
+			float y1 = vertex1[Y] - y;
+			float x2 = vertex2[X] - x;
+			float y2 = vertex2[Y] - y;
 
-		float a = (x1 * y2) - (x2 * y1);
-		float b = (x2 * y0) - (x0 * y2);
-		float c = (x0 * y1) - (x1 * y0);
+			float a = (x1 * y2) - (x2 * y1);
+			float b = (x2 * y0) - (x0 * y2);
+			float c = (x0 * y1) - (x1 * y0);
 
-		if (a >= 0 & b >= 0 & c >= 0) {
-			fragment.coordinates.set(iX, iY);
-			fragment.set(A, B, C, a / det, b / det, c / det);
-			m_cgPushFragment(fragment);
+			if (a >= 0 & b >= 0 & c >= 0) {
+				fragment.coordinates.set(iX, iY);
+				fragment.set(A, B, C, a / det, b / det, c / det);
+				m_cgPushFragment(fragment);
+			}
 		}
 	}
 }
