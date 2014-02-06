3ffbf52452c29f66a813d08361099bace48c1615
king-ton <tfreitag93@gmail.com>
2014-02-06 14:03:23 +0100

    Übung11 - Aufgabe2c - Samplen einer Textur an den gegebenen Koordinaten

--- CGTexture.cpp
+++ CGTexture.cpp
@@ -14,11 +14,13 @@ CGTexture2D::CGTexture2D()
 	wrapMode=CG_CLAMP;
 	data=NULL;
 }
+
 //---------------------------------------------------------------------------
 CGTexture2D::~CGTexture2D()
 {
 	free(data); data=NULL;
 }
+
 //---------------------------------------------------------------------------
 void CGTexture2D::allocate(int width, int height, unsigned char* data4ub)
 {
@@ -27,28 +29,59 @@ void CGTexture2D::allocate(int width, int height, unsigned char* data4ub)
 	data = (unsigned char*) realloc(data,width*height*4);
 	memcpy(data,data4ub,width*height*4);
 }
+
 //---------------------------------------------------------------------------
 void CGTexture2D::setFilterMode(int mode)
 {
 	filterMode = mode;
 }
+
 //---------------------------------------------------------------------------
 void CGTexture2D::setWrapMode(int mode)
 {
 	wrapMode = mode;
 }
+
+//---------------------------------------------------------------------------
+// Gibt Farbwert eines bestimmten Texels zurück
+//
+// Übung 11 - Aufgabe 2a  |  Funktion implementiert
 //---------------------------------------------------------------------------
 CGVec4 CGTexture2D::fetchTexel(int x, int y) const
 {
 	CGVec4 sample;
-	// ...
+	
+	if (wrapMode == CG_CLAMP) {
+		if (x >= width)
+			x = width - 1;
+		if (y >= height)
+			y = height - 1;
+	}
+	else if (wrapMode == CG_REPEAT) {
+		if (x >= width)
+			x = x % width;
+		if (y >= height)
+			y = y % height;
+	}
+
+	for (int i = 0; i < 4; i++)
+		sample.elements[i] = data[y * width * 4 + x * 4 + i] / 255;
+
 	return sample;
 }
+
+//---------------------------------------------------------------------------
+// Übung 11 - Aufgabe 2c  |  Funktion implementiert für CG_NEAREST
 //---------------------------------------------------------------------------
 CGVec4 CGTexture2D::sample(const CGVec4 &texcoord) const
 {
 	CGVec4 sample;
-	// ...
+
+	if (filterMode == CG_NEAREST) {
+		int x = texcoord.elements[0] * width;
+		int y = texcoord.elements[1] * height;
+		sample = fetchTexel(x, y);
+	}
+
 	return sample;
 }
-//---------------------------------------------------------------------------
